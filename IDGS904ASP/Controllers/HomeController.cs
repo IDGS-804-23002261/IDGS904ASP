7b0eb3ad54a8666b36c5b053ddeaf6c2eac656fc
IDGS-804-23002261 <anahigomezz2810@gmail.com>
2026-05-14 12:03:08 -0600

    Display 13/05/26

---
+++ IDGS904ASP/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("Index", "Pruebas");
+        }
+
+    
+        //// redireccionar a otra pagina 
+        //public RedirectResult Index()
+        //{
+        //    return Redirect("https://google.com.mx");
+
+        //}
+
+        //public ActionResult Index()
+        //{
+        //    var alumno1 = new Persona() { Nombre = "Jimena", Edad = 21, Empleado = true };
+        //    var alumno2 = new Persona() { Nombre = "Stefania", Edad = 20, Empleado = false };
+
+        //    //  return View();
+        //    return Json( new List<Persona>() {alumno1, alumno2}, JsonRequestBehavior.AllowGet);
+
+        //}
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
