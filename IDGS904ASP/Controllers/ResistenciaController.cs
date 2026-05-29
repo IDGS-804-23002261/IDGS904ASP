1045bc0939e916d3569bcc1b4bca405700891c66
IDGS-804-23002261 <anahigomezz2810@gmail.com>
2026-05-29 00:03:51 -0600

    Resistencia 27/05/26

---
+++ IDGS904ASP/Controllers/ResistenciaController.cs
@@ -0,0 +1,34 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class ResistenciaController : Controller
+    {
+        public static List<Resistencia>
+        listaResistencias = new List<Resistencia>();
+
+
+        [HttpGet]
+        public ActionResult Resistencia()
+        {
+            return View(listaResistencias);
+        }
+
+
+        [HttpPost]
+        public ActionResult Resistencia(Resistencia res)
+        {
+            res.Calcular();
+
+            listaResistencias.Clear();
+            listaResistencias.Add(res);
+
+            return View(listaResistencias);
+        }
+    }
+}
---
+++ IDGS904ASP/Models/Resistencia.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904ASP.Models
+{
+    public class Resistencia
+    {
+        public string Color1 { get; set; }
+        public string Color2 { get; set; }
+        public string Color3 { get; set; }
+        public string Tolerancia { get; set; }
+
+        public double Valor { get; set; }
+        public double Minimo { get; set; }
+        public double Maximo { get; set; }
+
+
+        public void Calcular()
+        {
+            int banda1 = ObtenerNumero(Color1);
+            int banda2 = ObtenerNumero(Color2);
+
+            int multiplicador = ObtenerMultiplicador(Color3);
+
+            Valor = ((banda1 * 10) + banda2) * multiplicador;
+
+            double tolerancia = 0;
+
+            if (Tolerancia == "Oro")
+            {
+                tolerancia = 0.05;
+            }
+            else
+            {
+                tolerancia = 0.10;
+            }
+
+            Minimo = Valor - (Valor * tolerancia);
+            Maximo = Valor + (Valor * tolerancia);
+        }
+
+
+        public int ObtenerNumero(string color)
+        {
+            switch (color)
+            {
+                case "Negro": return 0;
+                case "Cafe": return 1;
+                case "Rojo": return 2;
+                case "Naranja": return 3;
+                case "Amarillo": return 4;
+                case "Verde": return 5;
+                case "Azul": return 6;
+                case "Violeta": return 7;
+                case "Gris": return 8;
+                case "Blanco": return 9;
+            }
+
+            return 0;
+        }
+
+
+        public int ObtenerMultiplicador(string color)
+        {
+            switch (color)
+            {
+                case "Negro": return 1;
+                case "Cafe": return 10;
+                case "Rojo": return 100;
+                case "Naranja": return 1000;
+                case "Amarillo": return 10000;
+                case "Verde": return 100000;
+                case "Azul": return 1000000;
+                case "Violeta": return 10000000;
+                case "Gris": return 100000000;
+                case "Blanco": return 1000000000;
+            }
+
+            return 1;
+
+        }
+
+        public string ObtenerColorCss(string color)
+        {
+            switch (color)
+            {
+                case "Negro": return "black";
+                case "Cafe": return "brown";
+                case "Rojo": return "red";
+                case "Naranja": return "orange";
+                case "Amarillo": return "yellow";
+                case "Verde": return "green";
+                case "Azul": return "blue";
+                case "Violeta": return "purple";
+                case "Gris": return "gray";
+                case "Blanco": return "white";
+            }
+
+            return "white";
+        }
+    }
+}
